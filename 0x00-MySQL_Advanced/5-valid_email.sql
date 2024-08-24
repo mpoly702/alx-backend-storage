c1273eafc7564ffdd694fbd1be52a03014823db5
Godfrey Oriazowan <mpoly702@gmail.com>
2024-08-24 21:55:47 +0100

    Create 5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- creates a trigger that resets the attribute valid_email
+-- only when the email has been changed
+DROP TRIGGER IF EXISTS confirm_email;
+DELIMITER //
+CREATE TRIGGER confirm_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;
+//
+DELIMITER ;
